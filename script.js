deeadc26ac887b11bb01802662eec514c79c66dc
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2025-12-04 18:33:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-//your JS code here. If required.
+// your JS code here. If required.
 
+async function result() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+    const data = await response.json();
 
-async function result(){
-	const response=await fetch("https://jsonplaceholder.typicode.com/todos/1")
-	const jsonData=await response.json()
-	const data=jsonData.data
-
-	console.log({ userId: `${data.userId}`, id: `${data.id}`, title: `${data.title}`, completed: `${data.completed}` })
+    console.log({
+        userId: data.userId,
+        id: data.id,
+        title: data.title,
+        completed: data.completed
+    });
 }
 
-result()
+result();
