6de3f8b8a129b81ef0d2f37ac3bea409de219019
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2025-12-04 18:14:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+
+aysnc function result(){
+	const response=await fetch("https://jsonplaceholder.typicode.com/todos/1")
+	const jsonData=await response.json()
+
+	console.log(jsonData.data)
+}
