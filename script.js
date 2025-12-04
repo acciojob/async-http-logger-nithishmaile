0ce3ee3782f4b9f4a4fdc3fed30217e02ee783a7
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2025-12-04 18:29:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,9 @@
 async function result(){
 	const response=await fetch("https://jsonplaceholder.typicode.com/todos/1")
 	const jsonData=await response.json()
+	const data=jsonData.data
 
-	console.log(jsonData.data)
+	console.log({ userId: `${data.userId}`, id: `${data.id}`, title: `${data.title}`, completed: `${data.completed}` })
 }
 
 result()
